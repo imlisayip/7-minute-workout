b32dda9105d05cc502488be2453f4c69e4e7765f
imlisayip <imlisayip@gmail.com>
2021-02-03 10:46:59 -0800

    clean up + updating Frame component with pause interaction

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useRef } from 'react'
+import React from 'react'
 import { render } from "react-dom"
-import useSound from 'use-sound';
 import { useTimer } from 'react-timer-hook';
 
 import Begin from './Begin';
@@ -8,61 +7,30 @@ import Step from './Step';
 import Final from './Final';
 import Frame from './Frame';
 
-import female from './media/voiceover-female.ogg'
-import { exercises, steps, sprite } from './data'
-
-const TOTAL_EXERCISES = 12
-const MILLISECOND = 1000
-const EXERCISE_DURATION = 30
-const REST_DURATION = 10
+import { steps } from './data'
 
 const App = () => {
-    const [play, exposedData] = useSound(female, {
-        interrupt: false,
-        sprite: sprite
-    })
-
-
     return (
-
         <div className="App">
+            <button className="run" onClick={() => { }}>
+                <ion-icon name="play"></ion-icon>
+                                Start
+                            </button>
+            {steps.map((step, index) => {
+                return (
+                    <Frame step={step} key={index} />
+                )
+            })}
             <div
-                key="pause"
+                key="final"
                 className="frame"
-                id="pause"
+                id="final"
                 role="button"
                 tabIndex="0"
             >
-                <h1>Pause</h1>
+                <h1>Final</h1>
             </div >
-            {steps.map((step, index) => {
-                return (
-                    // <Frame step={step} key={step.key + index} />
-                    <div
-                        key={step.key + index}
-                        className="frame"
-                        id={step.type}
-                        role="button"
-                        tabIndex="0"
-                    >
-                        <h1>{step.step}</h1>
-                        <h2>{step.type}</h2>
-                        {/* {props.children} */}
-                        <button
-                            // key={step.audio}
-                            onClick={() => { play({ id: step.key }) }}
-                        >
-                            audioSprite
-                        </button>
-                        <button
-                            // key={step.audio}
-                            onClick={() => { play({ id: step.timer }) }}
-                        >
-                            counterSprite
-                        </button>
-                    </div >
-                )
-            })}
+
         </div >
     );
 
--- src/Frame.js
+++ src/Frame.js
@@ -1,22 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTimer } from 'react-timer-hook';
+import useSound from 'use-sound';
+import Clock from './Clock';
 
+import female from './media/voiceover-female.ogg'
+import { sprite } from './data'
 
 export default function Frame(props) {
-    // console.log(props)
+    const step = props.step
+    const [play] = useSound(female, {
+        interrupt: false,
+        sprite: sprite
+    })
+
+    const [isPaused, setIsPaused] = useState(false)
+    const [frame, setFrame] = useState(step.type)
+
+    const handlePause = () => {
+        if (isPaused) {
+            // unpause
+            setIsPaused(false)
+            setFrame(step.type)
+        } else {
+            // pause
+            setIsPaused(true)
+            setFrame('pause')
+        }
+    }
+
     return (
         <div
-            key={props.key}
             className="frame"
-            id={props.type}
+            id={frame}
             role="button"
             tabIndex="0"
+            onClick={handlePause}
+            onKeyDown={() => { }}
         >
-            <h1>{props.step}</h1>
-            <h2>{props.type}</h2>
-            {/* {props.children} */}
+            {isPaused ? (
+                <>
+                    <h1>currently</h1>
+                    <h3>{step.step}</h3>
+                    <p>Click anywhere to  resume workout</p>
+
+                    <button className="stop" onClick={() => { }}>
+                        <ion-icon name="stop"></ion-icon>
+                                Stop
+                </button>
+                </>
+            ) : (
+                    <>
+                        <h1>{step.step}</h1>
+                        <h2>{step.type}</h2>
+                        <button
+                            onClick={(e) => {
+                                e.stopPropagation()
+                                play({ id: step.key })
+                            }}
+                        >
+                            audioSprite
+            </button>
+                        <button
+                            onClick={(e) => {
+                                e.stopPropagation()
+                                play({ id: step.timer })
+                            }}
+                        >
+                            counterSprite
+            </button>
+                        <Clock countdown={props.elapsedTime} timer={step.elapsedTime / 1000} />
+                        <p>Click anywhere to pause workout</p>
+
+                    </>
+                )
+            }
+
+
+            {/* <p>Click anywhere to {isPaused ? "resume" : "pause"} workout</p> */}
 
         </div >
     );
 };
-
