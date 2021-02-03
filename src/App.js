6465175c19812d8052ae5c1221e75ed8bdbf8534
imlisayip <imlisayip@gmail.com>
2021-02-03 14:47:27 -0800

    add audio to one button in frame

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React from 'react'
 import { render } from "react-dom"
 import { useTimer } from 'react-timer-hook';
 
+import useSound from 'use-sound';
+import female from './media/voiceover-female.ogg'
+
 import Begin from './Begin';
 import Step from './Step';
 import Final from './Final';
 import Frame from './Frame';
 
-import { steps } from './data'
+import { steps, sprite } from './data'
 
 const App = () => {
+    const [play, exposedData] = useSound(female, {
+        interrupt: false,
+        sprite: sprite
+
+    })
     return (
         <div className="App">
-            <button className="run" onClick={() => { }}>
+            <button className="run" onClick={() => { play({ id: 'readyFor7MinuteIntro' }) }}>
                 <ion-icon name="play"></ion-icon>
                                 Start
                             </button>
